90b4bf94353df20a0760616f4b9aef85a944b8b5
Kyle Weicht <kyle@awesomekyle.com>
2013-03-30 10:25:20 -0700

    Small update

--- main.lua
+++ main.lua
@@ -1,3 +1,17 @@
--- Disable Dragon end caps
-MainMenuBarLeftEndCap:Hide()
-MainMenuBarRightEndCap:Hide()
+-- Set up frame to run on login
+local frame = CreateFrame("Frame");
+frame:RegisterEvent("PLAYER_LOGIN")
+
+frame:SetScript("OnEvent",function(self,event,id)
+    if(event == "PLAYER_LOGIN") then
+        -- Disable Dragon end caps
+        MainMenuBarLeftEndCap:Hide()
+        MainMenuBarRightEndCap:Hide()
+    end
+end)
+
+--
+-- Slash commands
+--
+SLASH_RL1 = "/rl";
+SlashCmdList["RL"] = function() ReloadUI() end
