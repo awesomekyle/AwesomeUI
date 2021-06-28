8c95da6f54fea489583360bae27de4d4ca9c3415
Kyle Weicht <kyle@awesomekyle.com>
2021-06-28 08:40:17 -0500

    create batch file to setup symbolic links

---
+++ mklink-awesomeui.bat
@@ -0,0 +1,26 @@
+@echo off
+set FILES=auraTracker.lua AwesomeUI.xml main.lua
+set VERSIONS=_classic_ _classic_era_ _classic_ptr_ _retail_ _ptr_
+
+for %%v in (%VERSIONS%) do (
+    set ADDON_PATH="%~dp0\..\%%v\Interface\AddOns\AwesomeUI"
+    if exist "%~dp0\..\%%v\" (
+        if not exist "%~dp0\..\%%v\Interface\AddOns\AwesomeUI" (
+            echo %~dp0\..\%%v\Interface\AddOns\AwesomeUI does not exist, creating it...
+            mkdir "%~dp0\..\%%v\Interface\AddOns\AwesomeUI"
+        ) else (
+            echo %~dp0\..\%%v\Interface\AddOns\AwesomeUI already exists
+        )
+        cmd /C mklink "%~dp0\..\%%v\Interface\AddOns\AwesomeUI\AwesomeUI.toc" "%~dp0\AwesomeUI.%%v.toc"
+        for %%f in (%FILES%) do (
+            cmd /C mklink "%~dp0\..\%%v\Interface\AddOns\AwesomeUI\%%f" "%~dp0\%%f"
+        )
+    )
+)
+
+@REM cmd /C mklink /D "%~dp0\_classic_\Interface\AddOns\AwesomeUI" "%~dp0\AwesomeUI"
+@REM cmd /C mklink /D "%~dp0\_classic_era_\Interface\AddOns\AwesomeUI" "%~dp0\AwesomeUI"
+@REM cmd /C mklink /D "%~dp0\_classic_ptr_\Interface\AddOns\AwesomeUI" "%~dp0\AwesomeUI"
+@REM cmd /C mklink /D "%~dp0\_retail_\Interface\AddOns\AwesomeUI" "%~dp0\AwesomeUI"
+@REM cmd /C mklink /D "%~dp0\_ptr_\Interface\AddOns\AwesomeUI" "%~dp0\AwesomeUI"
+@REM pause
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Readme
+
+## Installation Instructions
+
+This will setup proper symlinks to the various installed WoW versions (Live, PTR, Classic, etc), so
+any modifications to the base repo, will be automatically applied to the games.
+
+1. Clone repo into base `World of Warcraft` folder
+2. Run `mklink-awesomeui.bat`
