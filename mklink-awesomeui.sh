2eb2ea14a2b2ce0f08d7eb90d880d0ea6b54fac6
Kyle Weicht <kyle@awesomekyle.com>
2021-07-11 09:56:19 -0500

    remove unnecessary hardlink support

--- mklink-awesomeui.sh
+++ mklink-awesomeui.sh
@@ -4,15 +4,11 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 FILES=(auraTracker.lua AwesomeUI.xml main.lua auras.lua)
 VERSIONS=(_classic_ _classic_era_ _classic_ptr_ _retail_ _ptr_)
 
-HARD_LINK=false
 RESET=false
 
 for var in "$@"
 do
-    if [[ "$var" = '--hard' ]]; then
-        echo "Creating hard links"
-        HARD_LINK=true
-    elif [[ $var = '--reset' ]]; then
+    if [[ $var = '--reset' ]]; then
         echo "Deleting existing links first"
         RESET=true
     fi
@@ -32,17 +28,9 @@ for version in ${VERSIONS[*]}; do
         else
             echo "$ADDON_PATH already exists"
         fi
-        if [[ "$HARD_LINK" = true ]]; then
-            ln "$SCRIPT_DIR/AwesomeUI.$version.toc" "$ADDON_PATH/AwesomeUI.toc"
-        else
-            ln -s "$SCRIPT_DIR/AwesomeUI.$version.toc" "$ADDON_PATH/AwesomeUI.toc"
-        fi
+        ln -s "$SCRIPT_DIR/AwesomeUI.$version.toc" "$ADDON_PATH/AwesomeUI.toc"
         for file in ${FILES[*]}; do
-            if [[ "$HARD_LINK" = true ]]; then
-                ln "$SCRIPT_DIR/$file" "$ADDON_PATH/$file"
-            else
-                ln -s "$SCRIPT_DIR/$file" "$ADDON_PATH/$file"
-            fi
+            ln -s "$SCRIPT_DIR/$file" "$ADDON_PATH/$file"
         done
     fi
 done
